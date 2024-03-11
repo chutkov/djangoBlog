aa8aabb1e49e0586b7eb22e092e89201c65adaa2
vadim <vadikchutkov@gmail.com>
2024-03-11 19:52:13 +0500

    add modelManager "PublishedManager"

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,11 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 # Create your models here.
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset()\
+                      .filter(status=Post.Status.PUBLISHED)
+
 class Post(models.Model):
 
     class Status(models.TextChoices):
@@ -20,6 +25,10 @@ class Post(models.Model):
     status = models.CharField(max_length=2,
                               choices=Status.choices,
                               default=Status.DRAFT)
+    
+    objects = models.Manager()
+    published = PublishedManager()
+
 
     class Meta:
         ordering = ['-publish']
