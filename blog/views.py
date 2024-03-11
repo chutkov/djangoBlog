e0bd9d5a86124c588016ffa6f88e619aec69d2d2
vadim <vadikchutkov@gmail.com>
2024-03-11 21:51:59 +0500

    Adding a page-by-page breakdown to the post list view

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import Http404
+from django.core.paginator import Paginator
 from .models import Post
 
 # Create your views here.
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
     return render(request,
                   'blog/post/list.html',
                   {'posts': posts})
